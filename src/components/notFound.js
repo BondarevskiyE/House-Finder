fd8b62f888f4f13bed459a596582797f5232ffcc
EGOR <egorbond31@yandex.ru>
2020-03-11 14:09:10 +0300

    Update notFound.js

--- src/components/notFound.js
+++ src/components/notFound.js
@@ -4,12 +4,8 @@ import { Link } from "react-router-dom";
 export default class NotFound extends React.Component {
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
+      <div className="main">
             Страница не найдена. Вернуться на <Link to="/">главную</Link>?
-          </div>
-        </div>
       </div>
     );
   }
